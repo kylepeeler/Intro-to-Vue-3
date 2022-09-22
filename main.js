f3bd163195fc3af52974e3e54636b812a856dc93
Kyle Peeler <kylepeeler@hey.com>
2022-09-22 09:29:17 -0400

    finished lesson 2 coding challenge - add description

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  // ES6 shorthand
+  data() {
+    return {
+      product: "Socks",
+      description: "They make your feet very comfy and warm.",
+    };
+  },
+});
